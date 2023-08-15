c7522b9d2d39c841b109212f13cb61787371762f
Habib Ogunsola <ogunsolahabib@yahoo.com>
2023-08-15 16:01:21 +0100

    auto subscriptions

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,12 @@
 <script>
-  import { onMount } from "svelte";
   import ThemeWrapper from "./lib/ThemeWrapper.svelte";
   import { themeStore } from "./stores";
-
-  let setTheme;
-  let theme;
-  let themeStoreLocal;
-  themeStore.subscribe((val) => {
-    setTheme = val.setTheme;
-    theme = val.theme;
-    themeStoreLocal = { ...val };
-  });
-
-  // const { theme, setTheme } = $themeStore;
 </script>
 
-svelte
 <ThemeWrapper>
-  <p>Test</p>
-
   <div>
-    The current theme is: {theme}
-    <button on:click={() => setTheme("light")}>Light Mode</button>
-    <button on:click={() => setTheme("dark")}>Dark Mode</button>
+    The current theme is: {$themeStore.theme}
+    <button on:click={() => $themeStore.setTheme("light")}>Light Mode</button>
+    <button on:click={() => $themeStore.setTheme("dark")}>Dark Mode</button>
   </div>
 </ThemeWrapper>
